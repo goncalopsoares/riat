a106ea978e63a8bf9c280fd12c5e7e3f4349ddeb
goncalo <goncalo.soares@ua.pt>
2025-04-20 01:31:35 +0100

    generate spider chart

--- frontend/src/pages/Report.jsx
+++ frontend/src/pages/Report.jsx
@@ -1,25 +1,93 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from '../api';
+import Chart from "react-apexcharts";
 
 const Report = () => {
+
+    const [reportData, setReportData] = useState(null);
+    const [chartCategories, setChartCategories] = useState([]);
+    const [chartData, setChartData] = useState([]);
 
     const { id } = useParams();
 
     useEffect(() => {
         const getReport = async () => {
             const response = await api.get(`api/report/${id}/`);
-            console.log(response.data);
-
+            setReportData(response.data);
         }
-
         getReport();
+
     }, [id]);
+
+    const getChartCategories = () => {
+        if (reportData) {
+            const categories = reportData[0]?.dimension_scores_output.map(item => item.dimension_name);
+            setChartCategories(categories);
+            return categories;
+        }
+    }
+
+    const getChartData = () => {
+        if (reportData) {
+            const data = reportData[0]?.dimension_scores_output.map(item => item.reports_score_dimension_score);
+            setChartData(data);
+            return data;
+        }
+    }
+
+    useEffect(() => {
+        getChartCategories();
+        getChartData();
+    }, [reportData]);
+
+    const [options, setOptions] = useState({
+        chart: {
+            id: "basic-bar"
+        },
+        xaxis: {
+            categories: []
+        }
+    });
+
+    useEffect(() => {
+        setOptions((prevOptions) => ({
+            ...prevOptions,
+            xaxis: {
+                ...prevOptions.xaxis,
+                categories: chartCategories
+            }
+        }));
+    }, [chartCategories]);
+
+    const [series, setSeries] = useState([
+        {
+            name: "series-1",
+            data: []
+        }
+    ]);
+
+    useEffect(() => {
+        setSeries([
+            {
+                name: "series-1",
+                data: chartData
+            }
+        ]);
+    }, [chartData]);
 
     return (
         <>
             <div>
                 <h1>Report</h1>
+            </div>
+            <div className="mixed-chart">
+                <Chart
+                    options={options}
+                    series={series}
+                    type="radar"
+                    width="700"
+                />
             </div>
         </>
     );
