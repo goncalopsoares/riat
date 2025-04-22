6bfe23da93f90a1cd492079054f47abe2cc4b559
goncalo <goncalo.soares@ua.pt>
2025-04-22 16:12:03 +0100

    added dropdown for profile options

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
             <div className="container-fluid mx-2 my-4">
                 <a href="/" className="text-dark text-decoration-none">Responsible Innovation Assessment Tool</a>
                 {user && user.user_role === 1 ? (
-                    <ul className="navbar-nav d-flex flex-row">
+                    <ul className="navbar-nav d-flex flex-row me-3">
                         <li className="nav-item mx-2"><a className="nav-link" href="/surveytools">Assessments</a></li>
                         <li className="nav-item mx-2"><a className="nav-link" href="/">Users & Projects</a></li>
                         <li className="nav-item mx-2"><a className="nav-link" href="/">Reports & Data</a></li>
@@ -23,28 +23,28 @@ const Navbar = () => {
                             >
                                 <AccountCircleIcon />
                             </button>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="/">Profile</a></li>
-                                <li><a class="dropdown-item" href="/logout">Logout</a></li>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="/">Profile</a></li>
+                                <li><a className="dropdown-item" href="/logout">Logout</a></li>
                             </ul>
                         </li>
                     </ul>
                 ) : (
-                    <ul className="navbar-nav d-flex flex-row">
+                    <ul className="navbar-nav d-flex flex-row me-3">
                         <li className="nav-item mx-2"><a className="nav-link" href="/projects">Projects</a></li>
                         <li className="nav-item mx-2"><a className="nav-link" href="/assessment">Assessments</a></li>
                         <li className="nav-item mx-2"><a className="nav-link" href="/">Reports</a></li>
                         <li className="nav-item mx-2 dropdown">
                             <button
-                                className="btn btn-link nav-link d-flex align-items-center dropdown-toggle"
+                                className="btn btn-link nav-link d-flex align-items-center dropdown-toggle position-relative"
                                 data-bs-toggle="dropdown"
                                 aria-expanded="false"
                             >
                                 <AccountCircleIcon />
                             </button>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="/">Profile</a></li>
-                                <li><a class="dropdown-item" href="/logout">Logout</a></li>
+                            <ul className="dropdown-menu position-absolute">
+                                <li><a className="dropdown-item" href="/">Profile</a></li>
+                                <li><a className="dropdown-item" href="/logout">Logout</a></li>
                             </ul>
                         </li>
                     </ul>
