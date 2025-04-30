48ce8f84e653a7a071f3d5df8551b9ea039503f6
goncalo <goncalo.soares@ua.pt>
2025-04-30 11:38:47 +0100

    styles homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,43 +1,50 @@
 import { useNavigate } from 'react-router-dom';
+import '../styles/home.css';
 
 const Home = () => {
 
     const navigate = useNavigate();
 
     return (
-        <>
-            <div>
-                <h1>How responsible is your innovation?</h1>
-                <p>Take our assessment to find out how your innovation strategy compares to the world's most innovative companies.</p>
+        <div className='w-100 h-screen flex flex-col items-center justify-center' style={{ backgroundColor: '#f0f0f0' }}>
+            <div className='text-center mb-5' style={{ padding: '8rem 9.5rem 0 9.5rem' }}>
+                <h1 className='main-title'>How responsible is your innovation?</h1>
+                <p className='sub-title'>Take our assessment to find out how your innovation strategy compares to the world's most innovative companies.</p>
             </div>
-            <div>
-                <button onClick={() => navigate('/assessment/')}>
-                    <p>Start assessment</p>
+            <div className='my-5 w-100 text-center'>
+                <button onClick={() => navigate('/assessment/')}
+                    className='forms-button'>
+                    <p className='m-0'>Start assessment</p>
                 </button>
             </div>
-            <div>
+            <div className='w-100'>
                 <img
                     src="/landing_page_img.jpg"
                     alt="Innovation Assessment"
+                    className='w-100'
                 />
             </div>
-            <div>
-                <img src="/flag_eu.jpg"
-                    alt="Innovation Assessment"
-                />
-                <p>This project has received funding from the
-                    European Union’s Horizon Europe research
-                    and innovation programme under the
-                    Grant Agreement 101058385.
-
-                    Views and opinions expressed are however
-                    those of the authors only and do not
-                    necessarily reflect those of the European
-                    Union or Horizon Europe. Neither the
-                    European Union nor the granting authority
-                    can be held responsible for them.</p>
+            <div className='mt-5' style={{ display: 'grid', gridTemplateColumns: '1fr 2fr', alignItems: 'center' }}>
+                <div className='d-flex flex-row justify-content-center align-items-start'>
+                    <img src="/flag_eu.jpg"
+                        alt="Innovation Assessment"
+                        className='mx-4'
+                    />
+                    <p>This project has received funding from the
+                        European Union’s Horizon Europe research
+                        and innovation programme under the
+                        Grant Agreement 101058385.
+                        <br />
+                        <br />
+                        Views and opinions expressed are however
+                        those of the authors only and do not
+                        necessarily reflect those of the European
+                        Union or Horizon Europe. Neither the
+                        European Union nor the granting authority
+                        can be held responsible for them.</p>
+                </div>
             </div>
-        </>
+        </div>
     );
 };
 
