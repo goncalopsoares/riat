11b20f141c56f13c22c103b913458edcd91eba1b
Gabriel <hello@gabrielrolino.com>
2025-05-01 16:55:47 +0100

    Update sub-title and alt text in Home component for clarity

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <div className='w-100 h-screen flex flex-col items-center justify-center' style={{ backgroundColor: '#f0f0f0' }}>
             <div className='text-center mb-5' style={{ padding: '8rem 9.5rem 0 9.5rem' }}>
                 <h1 className='main-title'>How responsible is your innovation?</h1>
-                <p className='sub-title'>Take our assessment to find out how your innovation strategy compares to the world's most innovative companies.</p>
+                <p className='sub-title'>Take this self-assessment to find out how the path to full responsibility.</p>
             </div>
             <div className='my-5 w-100 text-center'>
                 <button onClick={() => navigate('/assessment/')}
@@ -20,7 +20,7 @@ const Home = () => {
             <div className='w-100'>
                 <img
                     src="/landing_page_img.png"
-                    alt="Innovation Assessment"
+                    alt="SoTecIn Factory"
                     className='w-100'
                 />
             </div>
