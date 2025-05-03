6e39bb27f97fa9486e29494893416520e9daa6f2
goncalo <goncalo.soares@ua.pt>
2025-05-03 23:00:50 +0100

    show user's answers on report

--- frontend/src/pages/Report.jsx
+++ frontend/src/pages/Report.jsx
@@ -196,6 +196,53 @@ const Report = () => {
                         />
                     </div>
                 </div>
+                <div>
+                    <h3 className="mt-5">Answers</h3>
+                    {dimensionsData && dimensionsData.length > 0 && !showAnswers && (
+                        <div className="answers-container">
+                            <h4>1. {dimensionsData[0].name}</h4>
+                            <p>{dimensionsData[0].description}</p>
+
+                            <div>
+                                {dimensionsData[0].statements.map((statement, index) => (
+                                    <div key={statement.id}>
+                                     
+                                            <div key={statement.id} className="my-5">
+                                                <b>1.{index + 1}. {statement.name}</b>
+                                                <p><em>{statement.description}</em></p>
+                                            </div>
+                                      
+                                        {
+                                            statement.answers.map(answer => (
+                                                <div key={answer.id} className="ml-4 text-sm text-gray-600">
+                                                    {statement.scale_labels.split(",").map((label, index) => (
+                                                        label === answer.value ? (
+                                                            <div key={index} className="d-inline-flex flex-column align-items-center">
+                                                                <div className="dot me-3"></div>
+                                                                <span className="scale-labels me-3 mb-1"><b style={{ color: '#0091be' }}>{label}</b></span>
+                                                            </div>
+                                                        ) : (
+                                                            <div key={index} className="d-inline-flex flex-column align-items-center">
+                                                                <div className="dot-invis"></div>
+                                                                <span className="scale-labels me-3 mb-1">{label}</span>
+                                                            </div>
+                                                        )
+                                                    ))}
+                                                </div>
+                                            ))
+                                        }
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    )}
+                </div>
+
+
+
+
+
+
                 {
                     showAnswers ? (
                         <button onClick={() => setShowAnswers(false)} className="forms-button">Hide Answers</button>
@@ -208,22 +255,33 @@ const Report = () => {
                         <div className="answers-container">
                             {dimensionsData.map((dimension, dimensionIndex) => (
                                 <div key={dimension.id} className="mb-4">
-                                    <h2>{dimensionIndex + 1}. {dimension.name}</h2>
+                                    <div className="d-flex flex-row justify-content-between align-items-center mb-2">
+                                        <h4>{dimensionIndex + 1}. {dimension.name}</h4>
+                                        <div className="d-flex flex-row align-items-center">
+                                            <div className="dot"></div>
+                                            <p className="m-0 ms-2"><b style={{ color: "#0091be" }}>Your answer</b></p>
+                                        </div>
+                                    </div>
                                     <p>{dimension.description}</p>
 
                                     {dimension.statements.map((statement, statementIndex) => (
-                                        <div key={statement.id} className="my-4">
+                                        <div key={statement.id} className="my-5">
                                             <b>{dimensionIndex + 1}.{statementIndex + 1}. {statement.name}</b>
                                             <p><em>{statement.description}</em></p>
 
                                             {statement.answers.map(answer => (
                                                 <div key={answer.id} className="ml-4 text-sm text-gray-600">
-                                                    <p className="m-0">Your answer:</p>
                                                     {statement.scale_labels.split(",").map((label, index) => (
                                                         label === answer.value ? (
-                                                            <span key={index} className="scale-labels me-3"><b style={{ color: '#0091be' }}>{label}</b></span>
+                                                            <div className="d-inline-flex flex-column align-items-center">
+                                                                <div className="dot me-3"></div>
+                                                                <span key={index} className="scale-labels me-3 mb-1"><b style={{ color: '#0091be' }}>{label}</b></span>
+                                                            </div>
                                                         ) : (
-                                                            <span key={index} className="scale-labels me-3">{label}</span>
+                                                            <div className="d-inline-flex flex-column align-items-center">
+                                                                <div className="dot-invis"></div>
+                                                                <span key={index} className="scale-labels me-3 mb-1">{label}</span>
+                                                            </div>
                                                         )
                                                     ))}
                                                 </div>
@@ -240,7 +298,7 @@ const Report = () => {
                     )
                 }
                 <div>
-                    <p>Score: {score} / {maxScore} </p>
+                    <h5 className="mt-5">Score: {score} / {maxScore} </h5>
                     <p>Overall Responsibility Level</p>
                     <p>{recommendationLevel}</p>
                     <p>The overall responsibility level is calculated based on the average score from the dimensions of the framework. The levels are based on the average score.</p>
