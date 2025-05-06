e963cf085dd4890c49766ab3e395c1c336d39007
goncalo <goncalo.soares@ua.pt>
2025-05-06 23:05:46 +0100

    fixed dropdown menu positioning

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,61 +11,61 @@ const Navbar = () => {
 
     return (
         <nav className="navbar sticky-top border-bottom border-secondary-subtle" style={{ zIndex: 5, height: "8rem", backgroundColor: "white" }}>
-                <a href="/" className="text-dark text-decoration-none ms-4">
-                    <span className="tool-title">
-                        Responsible Innovation Assessment Tool
-                        <br />
-                    </span>
-                    <span className="tool-subtitle">by INESC TEC</span>
-                </a>
-                {user && user.user_role === 1 ? (
-                    <ul className="navbar-nav d-flex flex-row me-4">
-                        <li className="nav-item mx-2 dropdown">
+            <a href="/" className="text-dark text-decoration-none ms-4">
+                <span className="tool-title">
+                    Responsible Innovation Assessment Tool
+                    <br />
+                </span>
+                <span className="tool-subtitle">by INESC TEC</span>
+            </a>
+            {user && user.user_role === 1 ? (
+                <ul className="navbar-nav d-flex flex-row me-4">
+                    <li className="nav-item mx-2 dropdown">
+                        <button
+                            className="btn btn-link nav-link d-flex align-items-center dropdown-toggle"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                        >
+                            <AccountCircleIcon />
+                        </button>
+                        <ul className="dropdown-menu dropdown-menu-end">
+                            <li><a className="dropdown-item" href="/">Profile</a></li>
+                            <li><a className="dropdown-item" href="/logout">Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            ) : (
+                <ul className="navbar-nav d-flex flex-row me-3">
+                    <li className="nav-item mx-2"><a className="nav-link" href="/projects">Projects</a></li>
+                    <li className="nav-item mx-2"><a className="nav-link" href="/assessment">Assessments</a></li>
+                    <li className="nav-item mx-2"><a className="nav-link" href="/">Reports</a></li>
+                    <li className="nav-item mx-2 dropdown">
+                        {user ? (
                             <button
-                                className="btn btn-link nav-link d-flex align-items-center dropdown-toggle"
+                                className="btn btn-link nav-link d-flex align-items-center dropdown-toggle position-relative"
                                 data-bs-toggle="dropdown"
                                 aria-expanded="false"
                             >
-                                <AccountCircleIcon />
+                                <AccountCircleIcon className="me-1" />
+                                {user && user.user_email}
                             </button>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="/">Profile</a></li>
-                                <li><a className="dropdown-item" href="/logout">Logout</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                ) : (
-                    <ul className="navbar-nav d-flex flex-row me-3">
-                        <li className="nav-item mx-2"><a className="nav-link" href="/projects">Projects</a></li>
-                        <li className="nav-item mx-2"><a className="nav-link" href="/assessment">Assessments</a></li>
-                        <li className="nav-item mx-2"><a className="nav-link" href="/">Reports</a></li>
-                        <li className="nav-item mx-2 dropdown">
-                            {user ? (
-                                <button
-                                    className="btn btn-link nav-link d-flex align-items-center dropdown-toggle position-relative"
-                                    data-bs-toggle="dropdown"
-                                    aria-expanded="false"
-                                >
-                                    <AccountCircleIcon />
+                        ) : (
+                            <div className="d-flex align-items-center">
+                                <button onClick={() => navigate('/login')} className="btn btn-secondary d-flex align-items-center">
+                                    Sign In
                                 </button>
-                            ) : (
-                                <div className="d-flex align-items-center">
-                                    <button onClick={() => navigate('/login')} className="btn btn-secondary d-flex align-items-center">
-                                        Sign In
-                                    </button>
-                                    <button onClick={() => navigate('/register')} className="btn btn-dark d-flex align-items-center ms-2">
-                                        Register
-                                    </button>
-                                </div>
-                            )}
-                            <ul className="dropdown-menu position-absolute">
-                                <li><a className="dropdown-item" href="/">Profile</a></li>
-                                <li><a className="dropdown-item" href="/logout">Logout</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                )
-                }
+                                <button onClick={() => navigate('/register')} className="btn btn-dark d-flex align-items-center ms-2">
+                                    Register
+                                </button>
+                            </div>
+                        )}
+                        <ul className="dropdown-menu dropdown-menu-end position-absolute" style={{ maxWidth: "8rem"}}>
+                            <li><a className="dropdown-item" href="/logout">Logout</a></li> 
+                        </ul>
+                    </li>
+                </ul>
+            )
+            }
         </nav >
     );
 };
