cb5681cae6fb181ca53ccaa42843ae4bca70b596
goncalo <goncalo.soares@ua.pt>
2025-05-15 03:23:20 +0100

    added new instructions text / added data policy document

--- frontend/src/components/AssessmentOne.jsx
+++ frontend/src/components/AssessmentOne.jsx
@@ -9,12 +9,18 @@ const AssessmentOne = ({ handleInstructionsRead }) => {
         <div className="global-container mt-5" style={{ minHeight: '60vh' }}>
             <div className="create-project-container">
                 <h2>Instructions</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi quisquam rerum cupiditate rem ut recusandae consequatur enim est itaque nostrum asperiores odio quia at omnis hic dolorum amet neque, maiores ducimus dignissimos tenetur laboriosam quo numquam. Placeat culpa architecto quidem ad, est rerum ipsum nostrum facilis! Quas, sapiente provident enim alias sit hic eaque impedit et rem non in illo totam id consectetur corrupti asperiores iure veniam fuga? Quia ratione, nulla cupiditate quas cumque at officiis sunt ducimus doloribus veritatis quis itaque voluptatibus illo quibusdam! Facilis neque error quia odio impedit nihil quidem cum eveniet inventore a, esse saepe totam.</p>
+                <p>The RIAT it should be conducted by the responsible of the innovation project in 3 different phases of the innovation project:</p>
+                <ul>
+                    <li>Phase 1 - At the beginning of the project planning while defining the scope and engaging the project team </li>
+                    <li>Phase 2- During project development (with at least 25% of project activities executed, before testing and it is recommended to repeat every 6 months)</li>
+                    <li>Phase 3 -At the end of the project, before launching the project results and go-to-market</li>
+                </ul>
+                <p>This self-assessment provides a set of statements by each of the Responsible Innovation dimensions to be scored in a seven-degree scale. Please use "Prefer not to answer" as last option. The overall assessment score level and final report wiil provide you the information of the dimensions to be improved and recommendations to be followed before future reassessments.</p>
                 {error && <p className="error-message"><ErrorIcon className="me-3" />{error}</p>}
                 <form className="form-container w-100 d-flex flex-column">
                     <label>
                         <input type="checkbox" id="instructions" name="instructions" />
-                        <span className="ms-2">Yes, I read the Instructions throughly and I am ready to start the assessment</span>
+                        <span className="ms-2">Yes, I read the instructions throughly and I am ready to start the assessment</span>
                     </label>
                     <div className="button-container mt-5 text-end">
                         <button onClick={handleInstructionsRead} disabled={loading} className='forms-button'>
--- frontend/src/components/AssessmentTwo.jsx
+++ frontend/src/components/AssessmentTwo.jsx
@@ -9,22 +9,20 @@ const AssessmentTwo = ({ handleAgreement }) => {
         <div className="global-container mt-5" style={{ minHeight: '60vh' }}>
             <div className="create-project-container">
                 <h2>Informed Consent & Data Privacy Agreement</h2>
-                <p>The RIAT (Tesponsible Innovation Assessment Tool) is a self assessment and reflexive tool that introduces accountability among innovators, encouraging a proactive approach to address potential risks and unintended consequences of their innovation projects.
-                    This is an interactive process by which  innovators assume a responsible approach towards innovation that creates change and positive impacts on society and the environment.
-                    It does not intent to provoque judgements, but  help reflect on impact and practices in a continuous improuvement mode. Though, it should be conducted by the responsible of the innovation project in 3 different phases of the project:</p>
+                <p>The RIAT tool has been developed as part of the  Horizon Europe SoTecIn Factory project and any personal data will be processed in accordance with applicable national and EU legislation and will only be used by researchers for the purposes of scientific research within the scope of the SoTecIn Factory project.</p>
                 {error && <p className="error-message"><ErrorIcon className="me-3" />{error}</p>}
                 <form className="form-container w-100 d-flex flex-column">
                     <label>
                         <input type="checkbox" id="agreement" name="agreement" />
-                        <span className="ms-2">Yes, I agree to the terms and conditions</span>
-                </label>
-                <div className="button-container mt-5 text-end">
-                    <button onClick={handleAgreement} disabled={loading} className='forms-button'>
-                        {loading ? "Submitting..." : "Next"}
-                    </button>
-                </div>
-            </form>
-        </div>
+                        <span className="ms-2">Yes, I've read the <a href="/privacy_policy_riat.pdf">Provicy Policy</a> throughly and I'm ready to continue</span>
+                    </label>
+                    <div className="button-container mt-5 text-end">
+                        <button onClick={handleAgreement} disabled={loading} className='forms-button'>
+                            {loading ? "Submitting..." : "Next"}
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div >
     )
 
