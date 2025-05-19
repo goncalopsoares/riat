9e42af339ff95577ee4c1ae4086fc8896238a5c7
goncalo <goncalo.soares@ua.pt>
2025-05-19 22:20:21 +0100

    removed color from assessment alert modal

--- frontend/src/components/AssessmentAlert.jsx
+++ frontend/src/components/AssessmentAlert.jsx
@@ -6,10 +6,10 @@ const AssessmentAlert = ({ show, setShow }) => {
     return (
         <div id="assessment-alert" className="modal">
             <div className="modal-content">
-                
+
                 <h3>RIAT says</h3>
                 <p>
-                    Please provide an answer to every statement and write an explanation for
+                    Please provide an answer to every statement and write an explanation for 
                     <em> 'Prefer not to answer'</em> options.
                 </p>
                 <a onClick={() => setShow(false)} className="close-btn" aria-label="Close alert">Close</a>
